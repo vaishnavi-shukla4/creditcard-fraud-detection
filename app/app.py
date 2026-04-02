73dff42982b37c8f23c889b480423eda9100d6c5
Vaishnavi Shukla <vaishnavishukla441@gmail.com>
2026-04-03 01:20:09 +0530

    moved models to same folder as app.py

--- app/app.py
+++ app/app.py
@@ -1,4 +1,3 @@
-import os
 import streamlit as st
 import pandas as pd
 import numpy as np
@@ -16,16 +15,12 @@ st.set_page_config(
 # ── Load model & scaler ───────────────────────────────────────────────────────
 @st.cache_resource
 def load_artifacts():
-    base_path = os.path.dirname(__file__)  # folder where app.py lives
-    model_path = os.path.join(base_path, "../models/model.pkl")
-    scaler_path = os.path.join(base_path, "../models/scaler.pkl")
-    features_path = os.path.join(base_path, "../models/features.pkl")
     
-    with open("../models/model.pkl", "rb") as f:
+    with open("model.pkl", "rb") as f:
         model = pickle.load(f)
-    with open("../models/scaler.pkl", "rb") as f:
+    with open("scaler.pkl", "rb") as f:
         scaler = pickle.load(f)
-    with open("../models/features.pkl", "rb") as f:
+    with open("features.pkl", "rb") as f:
         features = pickle.load(f)
     return model, scaler, features
 
